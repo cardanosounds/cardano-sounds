7dcf1323a83ee20fb7cf9258af97f8b530408db3
Dominik Zachar <dominik.zachar@gmail.com>
2021-06-18 01:12:33 +0200

    🚨 serve pwa files

--- apps/web/server.js
+++ apps/web/server.js
@@ -1,5 +1,7 @@
-const { createServer } = require("http");
-const next = require("next");
+const { createServer } = require("http")
+const { join } = require('path')
+const { parse } = require('url')
+const next = require("next")
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -8,7 +10,15 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    handle(req, res);
+    const parsedUrl = parse(req.url, true)
+      const { pathname } = parsedUrl
+      
+      if (pathname === '/sw.js' || /^\/(workbox|worker|fallback)-\w+\.js$/.test(pathname)) {
+        const filePath = join(__dirname, '.next', pathname)
+        app.serveStatic(req, res, filePath)
+      } else {
+        handle(req, res);
+      }
   }).listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on <http://localhost>:${port}`);
