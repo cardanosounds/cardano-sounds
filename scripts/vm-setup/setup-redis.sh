81cd52674ea5a3e91c98c03fa8e1054918a177f1
Dominik Zachar <dominik.zachar@gmail.com>
2021-06-29 17:55:09 +0200

    add script file to setup redis on bare ubuntu vm

---
+++ scripts/vm-setup/setup-redis.sh
@@ -0,0 +1,15 @@
+wget http://download.redis.io/redis-stable.tar.gz
+
+tar xvzf redis-stable.tar.gz
+
+cd redis-stable
+
+sudo apt-get install make
+
+sudo apt-get install gcc
+
+sudo apt-get install tcl
+
+sudo apt-get install build-essential
+
+sudo apt-get update
