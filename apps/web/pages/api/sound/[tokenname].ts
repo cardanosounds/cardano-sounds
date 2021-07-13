4a16c967f9f5a2fb56aea17dd4fa44e8f4b0d3d1
Dominik Zachar <dominik.zachar@gmail.com>
2021-07-13 23:32:05 +0200

    🚧 rename new api file

--- apps/web/pages/api/sound/[tokenname].ts
+++ apps/web/pages/api/sound/[tokenname].ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { NFTData } from '../../interfaces/interfaces'
+import { NFTData } from '../../../interfaces/interfaces'
 import { CosmosClient } from "@azure/cosmos"
 
 const endpoint = "https://your-account.documents.azure.com";
@@ -10,15 +10,15 @@ const containerid = "containerId"
 
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-	const body = req.body
+	const { tokenname } = req.query
 
-	if (!body) res.status(400).json("no id provided")
+	if (typeof(tokenname) === "undefined") res.status(400).json("no id provided")
 
 	let nftData: NFTData
 
-	const sound = await getSoundNFTData(JSON.parse(req.body))
-	if (typeof nftData === 'string' || nftData instanceof String){
+	const sound = await getSoundNFTData(tokenname as string)
 
+	if (typeof nftData === 'string' || nftData instanceof String){
 		res.status(404).json(sound)
 	}
 	else {
@@ -29,7 +29,6 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
 
 export async function getSoundNFTData(tokenName: string): Promise<NFTData | string>{
     if(!/[^a-zA-Z0-9]/.test(tokenName)){
-
         const database = (await client.databases.createIfNotExists({ id: databaseid })).database //client.database(databaseid).read()
         const container = (await database.containers.createIfNotExists({ id: containerid })).container //client.container(containerid).read()
         const res = await container.items
