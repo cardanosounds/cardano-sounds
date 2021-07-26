a52a6c07cc8b3b1db5a498fc598853500b7f66fb
Dominik Zachar <dominik.zachar@gmail.com>
2021-07-26 21:33:34 +0200

    🚧 logging, DI, written test

--- apps/Csharp.CardanoSounds/CS.TokenMint/Program.cs
+++ apps/Csharp.CardanoSounds/CS.TokenMint/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.IO;
 using CS.Csharp.CardanoCLI;
+using CS.Csharp.CardanoCLI.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging.Abstractions;
+using Serilog;
 //using Csharp.CardanoCLI;
 
 namespace CS.TokenMint
@@ -13,7 +18,45 @@ namespace CS.TokenMint
             //CLI cli = new CLI 
             var readyTx = DB.Cosmos.Transactions.GetReadyToMintTransaction();
 
+            var host = AppStartup();
 
+            var exampleService = ActivatorUtilities.CreateInstance<Examples>(host.Services);
+
+            exampleService.TestMintTokens();
+        }
+
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            // Check the current directory that the application is running on 
+            // Then once the file 'appsetting.json' is found, we are adding it.
+            // We add env variables, which can override the configs in appsettings.json
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        }
+
+        static IHost AppStartup()
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            // Specifying the configuration for serilog
+            Log.Logger = new LoggerConfiguration() // initiate the logger configuration
+                            .ReadFrom.Configuration(builder.Build()) // connect serilog to our configuration folder
+                            .Enrich.FromLogContext() //Adds more information to our logs from built in Serilog 
+                            .WriteTo.Console() // decide where the logs are going to be shown
+                            .CreateLogger(); //initialise the logger
+
+            Log.Logger.Information("Application Starting");
+            var host = Host.CreateDefaultBuilder() // Initialising the Host 
+                .ConfigureServices((context, services) => { // Adding the DI container for configuration
+                    services.AddSingleton<IExamples, Examples>(); 
+                })
+                .UseSerilog() // Add Serilog
+                .Build(); // Build the Host
+
+            return host;
         }
     }
 }
