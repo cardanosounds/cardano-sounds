d8f9b4d289afc28eb83abda5ba73dc374eefe942
zachyking <dominik.zachar@gmail.com>
2021-12-06 02:43:01 +0100

    fix weird build errors

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('next-pwa')
  
 module.exports = withPWA({
-    webpack(config) {
+    webpack: (config) => {
         config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
     
         // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
