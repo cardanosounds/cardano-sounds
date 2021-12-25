c83d2a05f928a301f9150a1ecfb013b90022b591
zachyking <dominik.zachar@gmail.com>
2021-12-25 04:41:56 +0100

    🚨 fix build error

--- apps/web/components/SoundNFTPreview.tsx
+++ apps/web/components/SoundNFTPreview.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NFTData } from '../interfaces/interfaces'
 import { 
 	Flex,
 	Stack,
@@ -8,8 +7,9 @@ import {
 	Spacer,
 } from '@chakra-ui/react'
 import SoundNFTPreviewSmall from './SoundNFTPreviewSmall'
+import { DatabaseTx } from '../interfaces/databaseTx'
 
-export default function SoundNFTPreview(soundNFTData: { nftData: NFTData } ) {
+export default function SoundNFTPreview(soundNFTData: { nftData: DatabaseTx } ) {
 	return (
 		<Flex
 			direction="row"
@@ -27,16 +27,15 @@ export default function SoundNFTPreview(soundNFTData: { nftData: NFTData } ) {
 				align="center"
 				justify="center"
 			>
-				
 				<Flex display="column">
-					<Heading size="sm">token name:</Heading><a><Text>{soundNFTData.nftData.metadata.token_name}</Text></a>
+					<Heading size="sm">token name:</Heading><a><Text>{soundNFTData.nftData.Metadata.token_name}</Text></a>
 					<Heading size="sm">policy:</Heading><Text>be3a4e111a307643783820c2bf15fcace87f161187be9301857b593a</Text>
-					<Heading size="sm">web:</Heading><a><Text>{soundNFTData.nftData.metadata.arweave_website_uri}</Text></a>
-					<Heading size="sm">rarity color:</Heading><Text>{soundNFTData.nftData.metadata.rarity}</Text>
-					<Heading size="sm">probability:</Heading><Text>{soundNFTData.nftData.metadata.probability} %</Text>
-					<Heading size="sm">sounds:</Heading>{soundNFTData.nftData.metadata.sounds.map(x => <p key={x.filename}>x.filename</p>)}
-					<Heading size="sm">player:</Heading><Text>{soundNFTData.nftData.metadata.player}</Text>
-					<Heading size="sm">buying tx:</Heading><Text>{soundNFTData.nftData.tx_Hash}</Text>
+					<Heading size="sm">web:</Heading><a><Text>{soundNFTData.nftData.Metadata.arweave_website_uri}</Text></a>
+					<Heading size="sm">rarity color:</Heading><Text>{soundNFTData.nftData.Metadata.rarity}</Text>
+					<Heading size="sm">probability:</Heading><Text>{soundNFTData.nftData.Metadata.probability} %</Text>
+					<Heading size="sm">sounds:</Heading>{soundNFTData.nftData.Metadata.sounds.map(x => <p key={x.filename}>x.filename</p>)}
+					<Heading size="sm">player:</Heading><Text>{soundNFTData.nftData.Metadata.player}</Text>
+					<Heading size="sm">buying tx:</Heading><Text>{soundNFTData.nftData.tx_hash}</Text>
 				</Flex>
 			</Stack>
 
