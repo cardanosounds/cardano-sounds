90fbc16aab54ee3e2fe31bc87f91898ba9c82d6e
zachyking <dominik.zachar@gmail.com>
2021-12-30 19:26:53 +0100

    update faulty previews gifs

--- player-previews/previewDict.py
+++ player-previews/previewDict.py
@@ -7,16 +7,16 @@ def imageForPlayer(player):
 		'randomdancers-dark' : 'QmQXF2bLQ2H5yZ87xiG3JSwpxm57dYBVVTVyWcD4Hq1WZX',
 		'randomdancers-light' : 'QmXnot5JhVqWFKweTRJgQg42b3KTEfH1czARi3gNzpmV1w',
 		'superformula-0-light' : 'QmdrQtaqpCGArAZw9oK1SJXt2BY1vwJohtCMeoQks8N5xA',
-		'superformula-0-dark' : 'Qmar2jVUG2C8HQt3azAgjfcAxTtduhUkr3WJjbvDRS9VpC',
+		'superformula-0-dark' : 'QmcG5g44D6LzUsVSFsyqD5QzkCmLidthPoo1AV6kkWiTYF',
 		'superformula-2-light' : 'QmfXy3WrdMfTpx7ZVMjwAfVwfzodhyETz7hyoimRE1pcYR',
-		'superformula-2-dark' : 'QmSSwjamkadKidbwtERBaLdSLGVVYo4xqaDsCoUHW7JuTx',
+		'superformula-2-dark' : 'QmQRsgeD2YCrScMqJmPtYhuTUGxDsXFx32wLekz4tmsjJK',
 		'superformula-4-light' : 'QmdKBFmAYHb1Y2WJhGLxNCa9qTwwQK4jYxvhv2q57YDwWf',
-		'superformula-4-dark' : 'QmX8WP319bRxj2AW9PUoVdmzcenMS5msDswCshugyS2zzR',
+		'superformula-4-dark' : 'QmS6WHneZZUzw36gCSjxtqHtVNx499Cz7NBP62fyoeyC2T',
 		'superformula-6-light' : 'QmcMUTjrPA4U48EBubuqmjKW5TQzxX8XuhvsqnsnbqgRyg',
-		'superformula-6-dark' : 'QmQEAg1PZPF18pgrhgrTZAKRFey5JhPCGjqUuSNHKVf6ny',
+		'superformula-6-dark' : 'QmNxoC1cYSidFDHHfrcAkGFLadKj8arf6PKjG8SZvGmqNY',
 		'superformula-8-light' : 'QmbJ3ZPiik15kagxmSZR5feoKMziZTDT9fqnBdLUkKyMoo',
-		'superformula-8-dark' : 'QmamExRzTDpvJatNzxGNEQL8tYm1Lm3TMkGHD79r1g7fej',
+		'superformula-8-dark' : 'QmdVKmB9w1LzagsNtZHaGnbx8hfbAU6qwb69tpQiU4AiCV',
 		'superformula-10-light' : 'Qmdu6BotLjJuMmGQiB6rHWnG7JEH4v8wpbM18j1Wf5fCKb',
-		'superformula-10-dark' : 'QmWxbiuZGNFx6aiXChihTfFS2ymY9Ttq8AJqPddoMnFdu5'
+		'superformula-10-dark' : 'QmfFptHUEpVhZFFSyWHT3egDA8rv4E4XHC8yHLcJG9h1vD'
 	}
 	return imagesDict[player]
