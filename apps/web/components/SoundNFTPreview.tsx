0cc0b0805ff8616a27ba5ebec1fcaff63f43f93d
zachyking <dominik.zachar@gmail.com>
2022-01-13 06:13:22 +0100

    mobile version preview

--- apps/web/components/SoundNFTPreview.tsx
+++ apps/web/components/SoundNFTPreview.tsx
@@ -14,13 +14,11 @@ export default function SoundNFTPreview(soundNFTData: { nftData: DatabaseTx } ) 
 		<Flex
 			direction={["column", "column", "row"]}
 			fontSize="xs"
-			w="75vw"
-			rounded="lg"
-			padding="1em"
+			w={["90vw", "90vw", "75vw"]}
 			mx="auto"
 			// _hover={{ boxShadow: "dark-lg", transform: "scale(1.1)", cursor: "pointer", zIndex: "999"}}
 		>	
-			<Flex p={10}>
+			<Flex p={[0,0,10]}>
 				<SoundNFTPreviewSmall soundNFTData={soundNFTData.nftData} fullView={true}/>
 			</Flex>
 			<Spacer></Spacer>
@@ -29,12 +27,13 @@ export default function SoundNFTPreview(soundNFTData: { nftData: DatabaseTx } ) 
 				align="center"
 				justify="center"
 				lineHeight={2}
+				px="1.5rem"
 			>
 				<Flex 
 					display="column"
 					wordBreak="break-all"
 				>
-					<Heading size="sm">token name:</Heading><a><Text>{soundNFTData.nftData.Metadata.token_name}</Text></a>
+					{/* <Heading size="sm">token name:</Heading><a><Text>{soundNFTData.nftData.Metadata.token_name}</Text></a> */}
 					<Heading size="sm">policy:</Heading><Text>be3a4e111a307643783820c2bf15fcace87f161187be9301857b593a</Text>
 					<Heading size="sm">web:</Heading><a href={soundNFTData.nftData.Metadata.arweave_website_uri}><Text>{soundNFTData.nftData.Metadata.arweave_website_uri}</Text></a>
 					<Heading size="sm">rarity color:</Heading><Text>{soundNFTData.nftData.Metadata.rarity}</Text>
