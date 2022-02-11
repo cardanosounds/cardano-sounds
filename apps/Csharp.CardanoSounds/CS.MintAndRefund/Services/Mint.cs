5a21978175802d0e1488f9ac7f990b182725d41b
zachyking <dominik.zachar@gmail.com>
2022-02-11 06:12:55 +0100

    update parameters and metadata creation for multimint

--- apps/Csharp.CardanoSounds/CS.MintAndRefund/Services/Mint.cs
+++ apps/Csharp.CardanoSounds/CS.MintAndRefund/Services/Mint.cs
@@ -41,51 +41,77 @@ namespace CS.MintAndRefund.Services
                 await Task.Delay(TimeSpan.FromSeconds(5));               
                 return;
             }
+            var metaStart = @"{
+  ""721"": {
+    ""97de3506172e572d4e7ba9874af2616c41ae3027c9894fde2c484a62"": ";
+
+            var metaEnd = @"},
+  ""20"" : {
+    ""327ebb172812c24e20a82db0d391b5403f58f305f672aed8fbd48808"": {
+        ""43534354"": {
+          ""icon"": ""https://app.cardanosounds.com/icons/csct.png"",
+          ""ticker"": ""CSCT"",
+          ""url"": ""https://cardanosounds.com"",
+          ""version"": ""1.0""
+        }
+    }
+  }
+}";
+            var nftMetaJsonString = File.ReadAllText(Path.Combine(_working_directory, "nftmetadatatemplate.json"));
+            var nftMeta = "";
             _logger.LogInformation("Started mint for " + tx.Id);
             _logger.LogInformation("1");
-            tx.Metadata.TokenName = "CSNFT" + tx.Id;
-            // CreateNFTPolicy();
-            _logger.LogInformation("2");
-
-            var meta = File.ReadAllText(Path.Combine(_working_directory, "metadatatemplate.json"));
-            meta = meta.Replace("NFT_NAME", tx.Metadata.TokenName);
-            meta = meta.Replace("IPFS_AUDIO", tx.Metadata.IpfsIdSound);
-            meta = meta.Replace("ARWEAVE_AUDIO", tx.Metadata.ArweaveIdSound);
-            meta = meta.Replace("SOUND_PROBABILITY", tx.Metadata.Probability.ToString());
-            meta = meta.Replace("IPFS_PLAYER_PREVIEW", tx.Metadata.PlayerImage);
-            meta = meta.Replace("PLAYER_NAME", tx.Metadata.Player);
-            meta = meta.Replace("RARITY_COLOR", tx.Metadata.Rarity);
-            _logger.LogInformation("3");
-            _logger.LogInformation(meta);
-            //char[]
-            string web = tx.Metadata.ArweaveWebsiteUri;
-            // string web = tx.Metadata.ArweaveWebsiteUri.Remove(0,5);
-            // web = web.Remove(web.Length - 7, 7);
-            meta = meta.Replace("ARWEAVE_WEBSITE", web);
-            meta = meta.Replace("TRANSACTION_HASH", tx.Tx_Hash);
-            for (var i = 0; i < tx.Metadata.Sounds.Length; i++)
+            foreach(var nft in tx.Metadata)
             {
-                meta = meta.Replace("SOUND_" + (i + 1), tx.Metadata.Sounds[i].Filename.Replace("/home/azureuser/cswaves/",""));
+                var meta = nftMetaJsonString;
+                nft.Metadata.TokenName = "CSNFT" + nft.Metadata.TokenName;
+                
+                // tx.Metadata.TokenName = "CSNFT" + tx.Id;
+                // CreateNFTPolicy();
+                // _logger.LogInformation("2");
+
+                meta = meta.Replace("NFT_NAME", tx.Metadata.TokenName);
+                meta = meta.Replace("IPFS_AUDIO", tx.Metadata.IpfsIdSound);
+                meta = meta.Replace("ARWEAVE_AUDIO", tx.Metadata.ArweaveIdSound);
+                meta = meta.Replace("SOUND_PROBABILITY", tx.Metadata.Probability.ToString());
+                meta = meta.Replace("IPFS_PLAYER_PREVIEW", tx.Metadata.PlayerImage);
+                meta = meta.Replace("PLAYER_NAME", tx.Metadata.Player);
+                meta = meta.Replace("RARITY_COLOR", tx.Metadata.Rarity);
+                // _logger.LogInformation("3");
+                // _logger.LogInformation(meta);
+                //char[]
+                string web = tx.Metadata.ArweaveWebsiteUri;
+                // string web = tx.Metadata.ArweaveWebsiteUri.Remove(0,5);
+                // web = web.Remove(web.Length - 7, 7);
+                meta = meta.Replace("ARWEAVE_WEBSITE", web);
+                meta = meta.Replace("TRANSACTION_HASH", tx.Tx_Hash);
+                for (var i = 0; i < tx.Metadata.Sounds.Length; i++)
+                {
+                    meta = meta.Replace("SOUND_" + (i + 1), tx.Metadata.Sounds[i].Filename.Replace("/home/azureuser/cswaves/",""));
+                }
+
+                meta.Replace("signatures/", "signatures: ");
+                meta.Replace("bass/", "bass: ");
+                meta.Replace("drums/", "drums: ");
+                meta.Replace("melodies/", "melody: ");
+                meta.Replace("enriching-mid-rare/", "enriching: ");
+                meta.Replace("enriching-rarest/", "enriching: ");
+                meta.Replace("enriching-common/", "enriching: ");
+            
+                if(nft != tx.Metadata.Last()) meta += @",
+        ";
+
+                nftMeta += meta;
             }
 
-            meta.Replace("signatures/", "signatures: ");
-            meta.Replace("bass/", "bass: ");
-            meta.Replace("drums/", "drums: ");
-            meta.Replace("melodies/", "melody: ");
-            meta.Replace("enriching-mid-rare/", "enriching: ");
-            meta.Replace("enriching-rarest/", "enriching: ");
-            meta.Replace("enriching-common/", "enriching: ");
-
-            _logger.LogInformation("4");
-
-            File.WriteAllText(Path.Combine(_working_directory, "metadata_" + tx.Metadata.TokenName + ".json"), meta);
+            File.WriteAllText(Path.Combine(_working_directory, "metadata_" + tx.Id + ".json"), 
+                metaStart + nftMeta + metaEnd);
 
             MintParams mintParams = CreateMintParamaters(tx);
 
             TransactionParams txParams = CreateTransactionParameters(tx);
 
             Assets assets = new Assets(_cli);
-            _logger.LogInformation("5");
 
             try 
             {
@@ -138,9 +164,8 @@ namespace CS.MintAndRefund.Services
                         Amount = new List<TokenValue>
                         {
                             new TokenValue(2000000),
-                            new TokenValue(1, EncodeStringToHex(tx.Metadata.TokenName)),
-                            new TokenValue(10, EncodeStringToHex("CSCT"))
-                        }
+                            new TokenValue(10 * tx.NftCount, EncodeStringToHex("CSCT"))
+                        }.Concat(tx.Metadata.Select(nft =>  new TokenValue(1, EncodeStringToHex(nft.TokenName))))
 
                     },
                     new TxOut
@@ -153,7 +178,7 @@ namespace CS.MintAndRefund.Services
                         }
                     }
                 },
-            MetadataFileName = $"metadata_{tx.Metadata.TokenName}.json",
+            MetadataFileName = $"metadata_{tx.Id}.json",
             SendAllTxsUnspentOutput = true,
             SigningKeyFile = _signing_key
         };
@@ -162,19 +187,19 @@ namespace CS.MintAndRefund.Services
         {
             TokenParams = new List<TokenParams>
                 {
-                    new TokenParams()
-                    {
-                        PolicyName = "c-sound-mainnet-nft-policy",
-                        TokenAmount = 1,
-                        TokenName = EncodeStringToHex(tx.Metadata.TokenName)
-                    },
                     new TokenParams
                     {
                         PolicyName = "c-sound-mainnet-ft-policy",
-                        TokenAmount = 10,
+                        TokenAmount = 10 * tx.NftCount,
                         TokenName = EncodeStringToHex("CSCT")
                     }
-                }
+                }.Concat(tx.Metadata.Select(nft =>  new TokenParams 
+                    { 
+                        PolicyName = "c-sound-mainnet-nft-policy",
+                        TokenAmount = 1,
+                        EncodeStringToHex(nft.TokenName)
+                    }
+                ));
         };
 
         private string EncodeStringToHex(string input)
