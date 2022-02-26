8ffa79969eb137029358ac5a9319dbea0d862f5f
zachyking <dominik.zachar@gmail.com>
2022-02-26 04:26:58 +0100

    change presale page

--- apps/web/components/PreBuy.tsx
+++ apps/web/components/PreBuy.tsx
@@ -46,11 +46,11 @@ export default function PreBuy(data) {//: {status: string, datetime: Date}) {
                             The collection is being released in 3 <strong>short</strong> waves
                     </ListItem>
                     <Spacer/>
-                    <ListItem>
+                    {/* <ListItem>
                         <ListIcon as={FaChevronRight} color="green.500" />
                             The following second wave will be open for <strong>24 hours</strong>
-                    </ListItem>
-                    {/* <ListItem>
+                    </ListItem> */}
+                    <ListItem>
                         <ListIcon as={FaChevronRight} color="green.500" />
                             When time is set, an address with QR code and price will be displayed here
                             <Tooltip 
@@ -65,7 +65,7 @@ export default function PreBuy(data) {//: {status: string, datetime: Date}) {
                                     onClick={() => setIsTooltipOpen(true)}
                                 />
                             </Tooltip>
-                    </ListItem> */}
+                    </ListItem>
                     <Spacer/>
                     <ListItem>
                         <ListIcon as={FaChevronRight} color="green.500" />
@@ -121,10 +121,11 @@ export default function PreBuy(data) {//: {status: string, datetime: Date}) {
                      justifyContent={["flex-start", "flex-start", "center"]}
                     >
                     <Countdown date={new Date(data.data.datetime)}>
-                        <NextChakraLink 
+                        {/* <NextChakraLink 
                             href="/buy"
-                        ><Button width="100%" h="100%">BUY<FaChevronRight /></Button>
-                        </NextChakraLink>
+                        > */}
+                            <Button width="100%" variant={'ghost'} h="100%">{new Date(data.data.datetime).getTime() < new Date().getTime() ? 'Next wave soon!' : "BUY"}<FaChevronRight /></Button>
+                        {/* </NextChakraLink> */}
                     </Countdown>
                     </Flex>
                     
