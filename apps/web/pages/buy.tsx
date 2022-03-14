2fb65414f0f3381461cd435616432fef6b759024
zachyking <dominik.zachar@gmail.com>
2022-03-14 21:07:46 +0100

    set next wave date

--- apps/web/pages/buy.tsx
+++ apps/web/pages/buy.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
-import React, { useEffect } from "react"
+// import React, { useEffect } from "react"
 import PreBuy from '../components/PreBuy'
 import BuyComp from '../components/Buy'
 import { GetServerSideProps } from 'next'
 
 export default function Buy(data){
-    useEffect(() => console.log(data), [data])
+    // useEffect(() => console.log(data), [data])
     return (
         <>
         
@@ -29,16 +29,16 @@ export default function Buy(data){
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const saleDateString = "2022-02-24 20:00:00 GMT+0100"
-    const saleEndDateString = "2022-02-25 20:00:00 GMT+0100"
+    const saleDateString = "2022-03-27 20:00:00 GMT+0100"
+    const saleEndDateString = "2022-03-30 20:00:00 GMT+0100"
     const saleDate =  new Date(saleDateString) 
     const saleEndDate =  new Date(saleEndDateString) 
 
     const dt = new Date()
     const addr = process.env.SALE_ADDRESS || "";
     const price = process.env.SALE_PRICE || 0;
+    
     let data
-  
     if(dt.getTime() > saleDate.getTime() && dt.getTime() < saleEndDate.getTime()) {
       data = { status: "open", address: addr, price: price }
     } else {
