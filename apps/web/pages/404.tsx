f48e2d4ea8bd1c390a2762a1491c296d6ceed5c8
zachyking <dominik.zachar@gmail.com>
2022-04-05 18:40:14 +0200

    improve 404

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -1,5 +1,13 @@
-export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
-  }
+import { Flex, Heading } from "@chakra-ui/react";
+import Layout from "../components/layout";
 
-  
+export default function Custom404() {
+  return (
+    <Layout>
+      <Flex align="center" justify="center" minH="85vh" mt="15vh" m="0">
+        <Heading>We couldn't find what you were looking for 😨</Heading>
+      </Flex>
+    </Layout>
+  )
+}
+
