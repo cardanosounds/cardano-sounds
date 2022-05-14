2e993560683db940066ebfd6ea6d2f5be0fcc8cb
zachyking <dominik.zachar@gmail.com>
2022-05-14 05:13:56 +0200

    arweave connection

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -6,8 +6,7 @@ import theme from '../styles/theme'
 import "@fontsource/share-tech-mono";
 import "font-awesome/css/font-awesome.css"
 import WalletContext from '../lib/WalletContext';
-
-// const TIMEOUT: number = 400
+import { ArjsProvider } from 'arjs-react'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [state, setState] = useState({
@@ -19,11 +18,20 @@ export default function App({ Component, pageProps }: AppProps) {
     setState(Object.assign({}, state, data));
   }
   return <>
-          <WalletContext.Provider value={state}>
-            <ChakraProvider theme={theme}>
-                <Component {...pageProps} 
-                />
-            </ChakraProvider>
-          </WalletContext.Provider>
-        </>
+    <ArjsProvider
+      //Add wallets here
+      connectors={{
+        arconnect: true,
+        arweave: true
+      }}
+      //enable/disable smartweave contract interaction here
+      enableSWC={false}>
+      <WalletContext.Provider value={state}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps}
+          />
+        </ChakraProvider>
+      </WalletContext.Provider>
+    </ArjsProvider>
+  </>
 }
